1f4abfa33d6e6bb3eec1c23a3d8be5756c583e58
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-05-21 05:53:48 +0900

    emailのvalidationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates :encrypted_password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
