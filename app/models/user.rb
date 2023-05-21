bc63ea420af9d797a5e66fe24e481912723c9810
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-05-22 05:48:24 +0900

    不要なバリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates :encrypted_password, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
