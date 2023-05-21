26aa5b1d40271f15441b2aac9791e5e8766e2f10
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-05-22 06:03:43 +0900

    パスワードは、半角英数字混合での入力が必須であることのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze }
   validates :encrypted_password, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
@@ -11,3 +12,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+
