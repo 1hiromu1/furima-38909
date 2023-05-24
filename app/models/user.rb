72b2fd4404a2047e0c8354abe9684253133c8bc6
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-05-24 15:42:43 +0900

    itemモデルとuserモデルのアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :items
+
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
