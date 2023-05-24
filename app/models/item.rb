16a91d6a150178ae0d9bd98de454e928626c524f
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-05-24 15:31:29 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,15 @@ class Item < ApplicationRecord
   belongs_to :delivery
   belongs_to :prefecture
   belongs_to :days_to_ship
+
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :delivery_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true
+
+  validates :category_id, :item_condition_id, :delivery_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
