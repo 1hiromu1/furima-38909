d5f47a0cc5c276e9bf5c7e9602443c9bc251957e
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-05-25 06:14:16 +0900

    itemモデルの登録出来る時のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
+
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it 'item_name,item_description,category_id,item_condition_id,delivery_id,prefecture_id,days_to_ship_id,price,imageが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
