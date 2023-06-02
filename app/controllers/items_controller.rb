fe9afcbc716bd1dd4ded09beedba032eaee1784a
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-02 21:08:23 +0900

    未ログイン時のページ遷移制限にdestroyを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :common_item, only: [:show, :edit, :update, :destroy]
 
   def index
