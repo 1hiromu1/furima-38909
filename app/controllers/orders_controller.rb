a1142e6cfdfbe997d424028ddbac59057bbc0504
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-16 06:44:52 +0900

    注文情報を保存するための記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,25 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_information = OrderInformation.new
   end
+
+  def new
+    @order_information = OrderInformation.new
+  end
+
+  def create
+    @order_information = OrderInformation.new(order_information_params)
+    if @order_information.valid?
+      @order_information.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_information_params
+    params.require(:order_information).permit(:item_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id)
+  end
+
 end
