e8ac8bc095d6bf0ba04f156cd386f5fb7fa10780
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-16 21:28:57 +0900

    購入機能のコントローラーの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,24 +4,21 @@ class OrdersController < ApplicationController
     @order_information = OrderInformation.new
   end
 
-  def new
-    @order_information = OrderInformation.new
-  end
-
   def create
+    @item = Item.find(params[:item_id])
     @order_information = OrderInformation.new(order_information_params)
     if @order_information.valid?
       @order_information.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_information_params
-    params.require(:order_information).permit(:item_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id)
+    params.require(:order_information).permit(:item_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
