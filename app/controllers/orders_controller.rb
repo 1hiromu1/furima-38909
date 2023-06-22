3ad0fa3ea9f90a2a3316b0a143bded7dc2eed1c5
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-22 11:19:12 +0900

    決済機能の導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_information = OrderInformation.new(order_information_params)
     if @order_information.valid?
+      pay_item
       @order_information.save
       redirect_to root_path
     else
@@ -19,6 +20,15 @@ class OrdersController < ApplicationController
 
   def order_information_params
     params.require(:order_information).permit(:item_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_information_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
