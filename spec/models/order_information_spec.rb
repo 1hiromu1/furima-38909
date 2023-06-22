9534f05222d3d6a7e340e38c340039955507bfdd
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-22 20:01:58 +0900

    ユーザーと商品のインスタンスを生成する様に記述

--- spec/models/order_information_spec.rb
+++ spec/models/order_information_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderInformation, type: :model do
   before do
-    @order_information = FactoryBot.build(:order_information)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item, user: user)
+    @order_information = FactoryBot.build(:order_information, user_id: user.id, item_id: item.id)
   end
 
   describe '商品購入機能' do
