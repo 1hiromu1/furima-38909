69a6c749971787f7b55c3d19fc8465d0eb9b7614
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-22 22:15:14 +0900

    バリデーションの追加

--- app/models/order_information.rb
+++ app/models/order_information.rb
@@ -7,7 +7,8 @@ class OrderInformation
     validates :user_id
     validates :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipalities
     validates :municipalities
     validates :address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
