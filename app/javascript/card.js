09b0bdc3b61b90b946bf6ce579051636a65315d0
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-22 11:18:52 +0900

    環境変数を適応させる記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_32d3627f8bc74c7cd2699442');
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
