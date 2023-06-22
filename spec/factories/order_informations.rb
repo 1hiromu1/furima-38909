b259bebb5c6d73c0fc2a8a97b9253b9a5031c109
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-06-22 20:01:40 +0900

    ファクトリーを修正

--- spec/factories/order_informations.rb
+++ spec/factories/order_informations.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_information do
-    user_id          { Faker::Number.between(from: 1, to: 10) }
-    item_id          { Faker::Number.between(from: 1, to: 10) }
     post_code        { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     prefecture_id    { Faker::Number.between(from: 2, to: 48) }
     municipalities   { Faker::Address.city }
@@ -9,5 +7,8 @@ FactoryBot.define do
     building_name    { Faker::Address.secondary_address }
     telephone_number { Faker::Number.number(digits: [10, 11].sample) }
     token            { 'tok_abcdefghijk00000000000000000' }
+
+    association :user
+    association :item
   end
 end
